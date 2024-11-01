fe804c5ad3ab198a0c7478e5b3fe6af75d303e25
FormedNumber95 <aketza200510@gmail.com>
2024-11-01 17:19:33 +0100

    eliminadas clases test y añadidos los cometarios javadoc
    eliminadas las clases test que no se usan y añadido comentarios de tipo javadoc al codigo
    01/11/2024

--- app/src/main/java/com/example/calculadora2/MainActivity.kt
+++ app/src/main/java/com/example/calculadora2/MainActivity.kt
@@ -5,20 +5,30 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculadora2.databinding.ActivityMainBinding
 
+/**
+ * Actividad principal de la aplicación de calculadora, que implementa operaciones básicas.
+ */
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var firstNumber=0.0
-    private var secondNumber=0.0
-    private var operation: String?=null
+    private var firstNumber = 0.0
+    private var secondNumber = 0.0
+    private var operation: String? = null
 
+    /**
+     * Inicializa la actividad, configurando el diseño y los listeners de clic para cada botón.
+     *
+     * @param savedInstanceState Si la actividad se está re-inicializando después de haber sido
+     * previamente cerrada, este Bundle contiene los datos más recientes.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        operation=null
+        operation = null
 
+        // Configuración de listeners de clic para cada botón
         binding.btn0.setOnClickListener(this)
         binding.btn1.setOnClickListener(this)
         binding.btn2.setOnClickListener(this)
@@ -38,8 +48,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.btnComa.setOnClickListener(this)
     }
 
+    /**
+     * Método que se ejecuta al hacer clic en uno de los botones de la calculadora.
+     *
+     * @param view La vista del botón que ha sido clicado.
+     */
     override fun onClick(view: View?) {
-        when(view){
+        when(view) {
             binding.btn0 -> onNumberPresses("0")
             binding.btn1 -> onNumberPresses("1")
             binding.btn2 -> onNumberPresses("2")
@@ -60,59 +75,82 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun onNumberPresses(number:String){
+    /**
+     * Maneja el evento cuando se presiona un número.
+     *
+     * @param number El número en formato de cadena.
+     */
+    private fun onNumberPresses(number: String) {
         renderScreen(number)
         checkOperation()
     }
 
-    private fun renderScreen(number:String){
-        val result:String = if(binding.screen.text=="0" && number!=",")
+    /**
+     * Actualiza la pantalla de la calculadora con el número ingresado.
+     *
+     * @param number El número en formato de cadena que se mostrará en la pantalla.
+     */
+    private fun renderScreen(number: String) {
+        val result: String = if (binding.screen.text == "0" && number != ",")
             number
         else
             "${binding.screen.text}$number"
 
-        binding.screen.text=result
+        binding.screen.text = result
     }
 
-    private fun checkOperation(){
-        if(operation==null)
-            firstNumber=binding.screen.text.toString().toDouble()
+    /**
+     * Comprueba la operación actual, asignando valores a los números correspondientes.
+     */
+    private fun checkOperation() {
+        if (operation == null)
+            firstNumber = binding.screen.text.toString().toDouble()
         else
-            secondNumber=binding.screen.text.toString().toDouble()
+            secondNumber = binding.screen.text.toString().toDouble()
     }
 
-    private fun onOperationPresed(operation:String){
-        this.operation=operation
-        firstNumber=binding.screen.text.toString().toDouble()
-
-        binding.screen.text="0"
+    /**
+     * Maneja el evento cuando se presiona un operador.
+     *
+     * @param operation El operador en formato de cadena.
+     */
+    private fun onOperationPresed(operation: String) {
+        this.operation = operation
+        firstNumber = binding.screen.text.toString().toDouble()
+        binding.screen.text = "0"
     }
 
-    private fun onEqualPressed(){
-        val result=when(operation){
+    /**
+     * Calcula el resultado de la operación y actualiza la pantalla.
+     */
+    private fun onEqualPressed() {
+        val result = when (operation) {
             "+" -> firstNumber + secondNumber
             "-" -> firstNumber - secondNumber
             "x" -> firstNumber * secondNumber
             "/" -> firstNumber / secondNumber
-            else ->0
+            else -> 0
         }
 
-        operation=null
-        firstNumber=result.toDouble()
-        try{
-            binding.screen.text=if(result.toString().endsWith(".0")){
-                result.toString().replace(".0","")
-            }else{
-                "%.2".format(result)
+        operation = null
+        firstNumber = result.toDouble()
+        try {
+            binding.screen.text = if (result.toString().endsWith(".0")) {
+                result.toString().replace(".0", "")
+            } else {
+                "%.2f".format(result)
             }
-        }catch (e:Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-    private fun onClearPressed(){
-        binding.screen.text="0"
-        firstNumber=0.0
-        secondNumber=0.0
+    /**
+     * Limpia la pantalla y restablece los valores de los números.
+     */
+    private fun onClearPressed() {
+        binding.screen.text = "0"
+        firstNumber = 0.0
+        secondNumber = 0.0
     }
 }
